34e1966fe685863fb6545ff1a193c1c5fcd79eaf
Sankofa70 <156853976+Sankofa70@users.noreply.github.com>
2024-04-25 15:26:32 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -27,23 +27,10 @@ WebMidi.outputs.forEach(function (output, num) {
 // A function that detects whether the Key Context setting is on or off. When on, it disables the code that allows user input for the Chord Quality
 
 //The commented code below is to run the program when Key Context is OFF. It worked before, but not right now.
-//A function that allows the script to react when a different quality is selected on the quality dropdown menu.
-
-// let quality = "major";
-
-// qualityName.addEventListener("change", function () {
-//   quality = qualityName.value;
-//   console.log(quality);
-// });
-
-// Let Key Map = False
-
-// Then, where your notes are made, make If (Key Map = True,) [do Regular stuff] Else, (Do context stuff)
 
 //This code also allows for key selection with the Key Context feature turned on. It assigns a MIDI note as the "root," which will later be compared to the user's inputed MIDI note to change chord quality automatically.
 
 function kcKeySelect() {
-  // keyContextKey.addEventListener("change", function () {
   keyContextSelection = keyContextKey.value;
   console.log(keyContextSelection);
   if (keyContextSelection == "C Major") {
@@ -72,7 +59,6 @@ function kcKeySelect() {
     root = 71;
   }
   console.log(root);
-  // });
 }
 
 //These functions act as switches for the Key Context function to toggle. It enables Key Context Key selection when the Key Context is set to On, and disables it when its set to Off
@@ -103,7 +89,7 @@ keyContext.addEventListener("change", function () {
   }
 });
 
-//A function that allows the quality select menu to work when Key Context is set to "off"
+//A function that allows the script to react when a quality is selected on the quality dropdown menu. (Assuming Key Context is Off)
 
 let quality = "major";
 
@@ -144,27 +130,6 @@ functionSelect.addEventListener("change", function () {
   }
   console.log(progGenOn);
 });
-
-// functionSelect.addEventListener("change", function () {
-//   progGenOn ? progressionGeneratorOn() : chordGeneratorOn();
-
-//   if (progGenOn == true) {
-//     console.log("The Progression Generator is On");
-//   } else if (keyContextToggle == false) {
-//     console.log("The Chord Generator is On");
-//   }
-// });
-
-// functionSelect.addEventListener("change", function () {
-//   progGenOn ? progressionGeneratorOn() : chordGeneratorOn();
-
-//   if (progGenOn == true) {
-//     .addEventListener("change", kcKeySelect);
-//   } else if (keyContextToggle == false) {
-//     console.log("Does this even work?");
-//     keyContextKey.removeEventListener("change", kcKeySelect);
-//   }
-// });
 
 // Add an event listener for the 'change' event on the input devices dropdown.
 // This allows the script to react when the user selects a different MIDI input device.
@@ -277,6 +242,10 @@ dropIns.addEventListener("change", function () {
       myInput.addListener("noteon", function (someMIDI) {
         // When a note on event is received, send a note on message to the output device.
         // This can trigger a sound or action on the MIDI output device.
+
+        //This is supposed to activate the Prgression Generator or Chord Generator function, depending on which is selected
+        progGenOn ? progressionGenerator : chordGenerator;
+
         console.log(
           `My note is ${someMIDI.note.identifier}, it is pitch ${someMIDI.note.number}, with a velocity of ${someMIDI.note.rawAttack}`
         );
@@ -287,93 +256,13 @@ dropIns.addEventListener("change", function () {
       myInput.addListener("noteoff", function (someMIDI) {
         // Similarly, when a note off event is received, send a note off message to the output device.
         // This signals the end of a note being played.
+        progGenOn ? progressionGenerator : chordGenerator;
 
         myOutput.sendNoteOff(midiProcess(someMIDI, quality));
       });
     }
-
-    progGenOn ? progressionGenerator : chordGenerator;
-
-    // After changing the input device, add new listeners for 'noteon' and 'noteoff' events.
-    // These listeners will handle MIDI note on (key press) and note off (key release) messages.
   });
 });
-
-// function progressionGenerator() {
-//   console.log("The Progression Generator is Working");
-//   dropIns.addEventListener("change", function () {
-//     // Before changing the input device, remove any existing event listeners
-//     // to prevent them from being called after the device has been changed.
-//     if (myInput.hasListener("noteon")) {
-//       myInput.removeListener("noteon");
-//     }
-//     if (myInput.hasListener("noteoff")) {
-//       myInput.removeListener("noteoff");
-//     }
-
-//     // Change the input device based on the user's selection in the dropdown.
-//     myInput = WebMidi.inputs[dropIns.value];
-
-//     myInput.addListener("noteon", function (someMIDI) {
-//       // When a note on event is received, send a note on message to the output device.
-//       // This can trigger a sound or action on the MIDI output device.
-//       console.log(`My note is number ${someMIDI.note.number}`);
-//       //Based on the value of the MIDI note number compared to the root of the selected key's note number, change the quality of the chord. For now, this will only work with MIDI notes 60 - 72.
-//       //This also prevents the user from playing notes outside of their selected scale (Within the MIDI Note 60-72 Octave)
-
-//       if (keyContextToggle == true) {
-//         if (someMIDI.note.number == root) {
-//           quality = "major";
-//         } else if (someMIDI.note.number == root + 1) {
-//           quality = null;
-//         } else if (someMIDI.note.number == root + 2) {
-//           quality = "minor";
-//         } else if (someMIDI.note.number == root + 3) {
-//           quality = null;
-//         } else if (someMIDI.note.number == root + 4) {
-//           quality = "minor";
-//         } else if (someMIDI.note.number == root + 5) {
-//           quality = "major";
-//         } else if (someMIDI.note.number == root + 6) {
-//           quality = null;
-//         } else if (someMIDI.note.number == root + 7) {
-//           quality = "major";
-//         } else if (someMIDI.note.number == root + 8) {
-//           quality = null;
-//         } else if (someMIDI.note.number == root + 9) {
-//           quality = "minor";
-//         } else if (someMIDI.note.number == root + 10) {
-//           quality = null;
-//         } else if (someMIDI.note.number == root + 11) {
-//           quality = "minor";
-//         } else if (someMIDI.note.number == root + 12) {
-//           quality = "major";
-//         }
-//       }
-//     });
-
-//     // After changing the input device, add new listeners for 'noteon' and 'noteoff' events.
-//     // These listeners will handle MIDI note on (key press) and note off (key release) messages.
-//     myInput.addListener("noteon", function (someMIDI) {
-//       // When a note on event is received, send a note on message to the output device.
-//       // This can trigger a sound or action on the MIDI output device.
-//       console.log(
-//         `My note is ${someMIDI.note.identifier}, it is pitch ${someMIDI.note.number}, with a velocity of ${someMIDI.note.rawAttack}`
-//       );
-
-//       myOutput.sendNoteOn(midiProcess(someMIDI, quality));
-//     });
-
-//     myInput.addListener("noteoff", function (someMIDI) {
-//       // Similarly, when a note off event is received, send a note off message to the output device.
-//       // This signals the end of a note being played.
-
-//       myOutput.sendNoteOff(midiProcess(someMIDI, quality));
-//     });
-//   });
-// }
-
-// progGenOn ? progressionGenerator : chordGenerator;
 
 //An inefficient way of generating four MIDI notes from one, using the MIDI Note Number, and a quality assignment
 
