6adc114dc2d0860ba91d42f29822c4e04c9f1e4f
Sankofa70 <156853976+Sankofa70@users.noreply.github.com>
2024-05-03 00:46:19 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 // This is necessary to work with MIDI devices in the web browser.
 await WebMidi.enable();
 
+//Declares various objects to be used in the code
+
 let myInput = WebMidi.inputs[0];
 let myOutput = WebMidi.outputs[0].channels[1];
 
@@ -30,11 +32,10 @@ WebMidi.outputs.forEach(function (output, num) {
   dropOuts.innerHTML += `<option value=${num}>${output.name}</option>`;
 });
 
-// A function that detects whether the Key Context setting is on or off. When on, it disables the code that allows user input for the Chord Quality
+//The following series of sections declare and run functions that assign values to a number of objects that the chord and progression generators use to generate chords.
+//These functions include clarifying what note the chord generator should use as a root for the chord, the tempo of a chord progression, the quality of a chord, etc.
 
-//The commented code below is to run the program when Key Context is OFF. It worked before, but not right now.
-
-//This code also allows for key selection with the Key Context feature turned on. It assigns a MIDI note as the "root," which will later be compared to the user's inputed MIDI note to change chord quality automatically.
+//This function also allows for key selection with the Key Context feature turned on. It assigns a MIDI note as the "root," which will later be compared to the user's inputed MIDI note to change chord quality automatically.
 
 function kcKeySelect() {
   keyContextSelection = keyContextKey.value;
@@ -66,7 +67,14 @@ function kcKeySelect() {
   }
 }
 
-//These functions act as switches for the Key Context function to toggle. It enables Key Context Key selection when the Key Context is set to On, and disables it when its set to Off
+//This declaration sets the default key for the Key Context mode to C Major
+let keyContextSelection = "C Major";
+
+//This declares the "root of the chord" object to be used later
+let root;
+
+//These functions act as switches for the Key Context function to toggle.
+//It adds a listener to enable Key Context Key Selection selection when the Key Context is set to On, and removes the listener when its Off
 let keyContextToggle = false;
 
 function kcToggleOn() {
@@ -93,7 +101,9 @@ keyContext.addEventListener("change", function () {
   }
 });
 
-//A function that allows the script to react when a quality is selected on the quality dropdown menu. (Assuming Key Context is Off)
+//A function that allows a chord to be assigned a quality via quality menu selection when Key Context is off
+//When key context is on and a user attempts to change the quality, an error message is intentionally created by trying to run "null"
+//This prevents the user from changing a chord quality manually while Key Context is on, as the quality change process happens automatically
 
 let quality = "major";
 
@@ -105,12 +115,9 @@ function qualitySelect() {
 qualityName.addEventListener("change", function () {
   keyContextToggle ? null() : qualitySelect();
 });
-//Declarations for the following function, assuming Key Context is ON.
 
-let keyContextSelection = "C Major";
-let root;
-
-//These functions allow the program to react differently based on if the Chord Generator or the Progression Generator is activated
+//These functions allow the program to run two different systems based on if the Chord Generator or the Progression Generator is activated
+//The Chord Generator is the basic process of the software, and the Progression Generator simply adds a layer of math on top of it that runs it four times in a loop.
 
 let progGenOn = false;
 
@@ -127,12 +134,14 @@ function chordGeneratorOn() {
   console.log(progGenOn);
 }
 
-let chordOneSelection;
-let chordTwoSelection;
-let chordThreeSelection;
-let chordFourSelection;
+//These are declarations for objects in the Progression Generator. These are the four chords that users can change in a progression
+let chordOneSelection = "I";
+let chordTwoSelection = "I";
+let chordThreeSelection = "I";
+let chordFourSelection = "I";
 
-//These functions adds event listeners for the chord boxes in the Progression Generator
+//These functions add event listeners for the chord boxes in the Progression Generator section of the page
+//First, functions that tie the value on the html to a "selection" object are declared
 
 function chordOneAssigner() {
   chordOneSelection = chordOne.value;
@@ -154,7 +163,8 @@ function chordFourAssigner() {
   console.log(`The fourth chord in the progression is ${chordFourSelection}`);
 }
 
-// Add event listeners
+// Then, these functions are given event listeners when a "progression selection" function is run.
+// This function happens when the Progression Generator is turned on
 function progressionSelection() {
   chordOne.addEventListener("change", chordOneAssigner);
   chordTwo.addEventListener("change", chordTwoAssigner);
@@ -162,7 +172,8 @@ function progressionSelection() {
   chordFour.addEventListener("change", chordFourAssigner);
 }
 
-// Remove event listeners
+// This function runs when the Progression Generator is turned off. It removes the listeners for the chord progression boxes,
+// So they can't be changed while the Chord Generator is working
 function progressionSelectionDeactivate() {
   chordOne.removeEventListener("change", chordOneAssigner);
   chordTwo.removeEventListener("change", chordTwoAssigner);
@@ -170,6 +181,7 @@ function progressionSelectionDeactivate() {
   chordFour.removeEventListener("change", chordFourAssigner);
 }
 
+//This function is just saying "Turn on the chord boxes when the Progression Generator is on, then turn them off when the Chord Generator is on."
 functionSelect.addEventListener("change", function () {
   if (functionSelect.value == "progressionGen") {
     progGenOn = true;
@@ -182,10 +194,12 @@ functionSelect.addEventListener("change", function () {
   }
 });
 
-//A function for adjusting the tempo of the chord progression
+//Objects and functions for adjusting the tempo of the chord progression. The user inputs a tempo in BPM, and it is then converted into
+//milliseconds to be used later by a sequencer function
 
-let tempoInput = 120;
-let tempoInMilliseconds;
+let tempoInput;
+tempo.value = 120;
+let tempoInMilliseconds = 2000;
 
 function tempoToMilliseconds(tempo) {
   return 60000 / tempo;
@@ -195,10 +209,12 @@ tempo.addEventListener("change", function () {
   tempoInput = tempo.value;
   tempoInMilliseconds = tempoToMilliseconds(tempoInput) * 4;
   console.log(`The tempo of the chord progression is ${tempoInput} BPM.`);
-  console.log(
-    `The tempo of the chord progression in milliseconds is ${tempoInMilliseconds} BPM.`
-  );
+  console.log(`A note will play every ${tempoInMilliseconds} milliseconds.`);
 });
+
+//This is the end of the series of sections that deals with assigning values to things based on what the user does on the page
+// The following section concerns what happens whenever the user plays a MIDI note, and which code to run if either the Chord Generator
+// or Progression generator is on.
 
 // Add an event listener for the 'change' event on the input devices dropdown.
 // This allows the script to react when the user selects a different MIDI input device.
@@ -216,15 +232,20 @@ dropIns.addEventListener("change", function () {
   // Change the input device based on the user's selection in the dropdown.
   myInput = WebMidi.inputs[dropIns.value];
 
-  let intervalID;
+  //The following section is a massive if statement runs two different codes based on if the Progression Generator is on or off.
+  // The first part of the code is for when the Progression Generator is ON
+  // A tempoInterval object is declared for use later. This will specify at what times to send note on and off messages
+  let tempoInterval;
 
   if (progGenOn == true) {
     myInput.addListener("noteon", function (someMIDI) {
-      let currentRootNumber; // Define currentRootNumber here
+      let currentRootNumber;
 
-      // The function that specifies the root of chords that the Chord Generator should play based on user selections
+      // This function specifies the root of chords that the Chord Generator should play based on user selections on the page.
+      // This function works based off of what MIDI pitch the user has held down on their controller. It changes the pitch values being sent
+      // to the actual chord generator into other values based on the chord selection, so the user can get up to four different chords that
+      // have completely different roots than the note they're currently holding down.
       function progressionLooper() {
-        // Define the functions to be executed in order
         function firstChord() {
           if (chordOneSelection == "I") {
             currentRootNumber = someMIDI.note.number;
@@ -297,7 +318,12 @@ dropIns.addEventListener("change", function () {
           }
         }
 
-        // Define the sequence of function calls with delays
+        //The following function runs the chord functions specified above, but it does so in a loop, so when the firstChord function finishes,
+        //The secondChord function begins. The "tempoInMilliseconds" object makes its reappearance from just above this giant "Note On" function, so
+        //that the root of the chord changes in time with the tempo the user sets. However, by subtracting the time that this change happens by just a
+        //millisecond before the note on message is sent, the root note will always be adjusted in time for the note on message
+        //to be sent.
+
         function chordSequence() {
           setTimeout(function () {
             firstChord();
@@ -307,7 +333,7 @@ dropIns.addEventListener("change", function () {
                 thirdChord();
                 setTimeout(function () {
                   fourthChord();
-                  // Call the function recursively to loop indefinitely
+                  // When the fourthChord function is called, the chordSequence parent function is also called, so that the process can loop.
                   chordSequence();
                 }, tempoInMilliseconds - 1);
               }, tempoInMilliseconds - 1);
@@ -315,67 +341,71 @@ dropIns.addEventListener("change", function () {
           }, tempoInMilliseconds - 1);
         }
 
-        // Start the sequence
         chordSequence();
       }
 
       progressionLooper();
 
-      // Define a function to send note-on messages
+      // This function sends note on messages to the code that generates chords. It is modified compared to when the Progression Generator
+      // is off. It allows for the MIDI pitch number of the note being sent to be tweaked by the progression looper.
       function sendNoteOn() {
-        // Create a new MIDI message object with the desired note number
         let myNotes = {
           note: {
             identifier: someMIDI.note.identifier,
-            number: currentRootNumber, // Set the note number
+            number: currentRootNumber, // Sets the note number using the Root Number specified by the progression looper.
             rawAttack: someMIDI.note.rawAttack,
           },
         };
-        console.log(myNotes.note.number); // Log the current note number
+        console.log(myNotes.note.number); // Logs the note number of the note being sent
 
-        // Process the MIDI message with the updated note number
+        // Sends the Note Number to the midiPocess (AKA the chord generator code)
         myNotes = midiProcess(myNotes, quality);
 
-        // Send the note-on message
+        // Sends the package of four note on messages to the MIDI output
         myOutput.sendNoteOn(myNotes);
       }
 
-      // Set an interval to repeatedly send note-on messages and store its ID
-      intervalID = setInterval(sendNoteOn, tempoInMilliseconds);
+      // This runs the sendNoteOn function at an interval based on the info from the tempo change function.
+      // i.e, user changes tempo, the notes get sent faster or slower
+      tempoInterval = setInterval(sendNoteOn, tempoInMilliseconds);
     });
-
+    // With this function, when a note off message is sent by the user lifting their finger from a key, the loop of sendNoteOn messages is stopped
     myInput.addListener("noteoff", function (someMIDI) {
-      // Clear the interval set in the "noteon" section
-      clearInterval(intervalID);
+      clearInterval(tempoInterval);
     });
   } else {
+    //THIS is the start of the "send a note" code if the progression generator is OFF. It's way smaller by comparison.
     myInput.addListener("noteon", function (someMIDI) {
-      // When a note on event is received, send a note on message to the output device.
-      // This can trigger a sound or action on the MIDI output device.
-
+      // This function doesn't even mess with MIDI note values. Whatever the pitch of the note that the user plays is, it gets shipped off to the
+      //chord generator code as is. No timing stuff either, it just happens as soon as a note is played.
       let myNotes = midiProcess(someMIDI, quality);
       console.log(myNotes);
 
       myOutput.sendNoteOn(myNotes);
-
-      // });
     });
     myInput.addListener("noteoff", function (someMIDI) {
-      // Similarly, when a note off event is received, send a note off message to the output device.
-      // This signals the end of a note being played.
+      //And here, a note off message is sent to the MIDI output when the user sends a note off message by lifting their finger.
 
       myOutput.sendNoteOff(midiProcess(someMIDI, quality));
     });
   }
 });
 
-//An inefficient way of generating four MIDI notes from one, using the MIDI Note Number, and a quality assignment
+//This is the end of the "Note on" part of the code
+//Below is the the code where singular MIDI pitch values are turned into four different pitches (based on quality,) to be packaged up in an array
+//and spit back out to be sent to the MIDI output.
 
 const midiProcess = function (midiIN, quality) {
   let pitch = midiIN.note.number;
 
-  //Based on the value of the MIDI note number compared to the root of the selected key's note number, change the quality of the chord. For now, this will only work with MIDI notes 60 - 72.
-  //This also prevents the user from playing notes outside of their selected scale (Within the MIDI Note 60-72 Octave)
+  //This if statement runs two very similar codes based on if the progression generator option is on or off on the webpage. The only difference is
+  //that if the progression generator is on, the user cannot turn on "keyContextToggle", which would interfere with the code by changing chord
+  //qualities that don't need to be changed. When the progression generator isn't set to on, keyContextToggle is available, and will also let the user
+  //select the quality of their individual chord when keyContextToggle is off.
+
+  //Basically, this code reads the incoming MIDI pitch value, and changes the quality object to match what a chord would be within the context of a specific
+  //scale. So, for example, when the key is set to A major and a B pitch comes into the function, it sets the quality to "minor" because a
+  //B chord would be a minor ii in the key of A major. This quality is then read later in this code.
   if (progGenOn == true) {
     console.log("The Progression Generator is Working");
     keyContextToggle = true;
@@ -445,8 +475,8 @@ const midiProcess = function (midiIN, quality) {
     }
   }
 
-  //Put all outcome code in here
-  //Don't use function in here--All one big function
+  //Finally, where the magic happens. A series of four notes are created and outputted by the midiProcess function based on the root note's pitch
+  //and what the quality value is at that given moment.
 
   if (quality == "major") {
     let myNewNote1 = new Note(pitch, { rawAttack: 101 });
@@ -493,21 +523,27 @@ const midiProcess = function (midiIN, quality) {
   }
 };
 
-console.log(myOutput);
-// Add an event listener for the 'change' event on the output devices dropdown.
+//This is the end of the midiProcess part of this code.
+
+// To sum it up, this entire code is just: Define objects based on user input, send a note to a midiProcess function, and together with the
+// note and all of the defined objects, generate four notes to be sent to the MIDI output
+
+// The function below adds event listener for the 'change' event on the output devices dropdown.
 // This allows the script to react when the user selects a different MIDI output device.
 dropOuts.addEventListener("change", function () {
   // Change the output device based on the user's selection in the dropdown.
   // The '.channels[1]' specifies that the script should use the first channel of the selected output device.
-  // MIDI channels are often used to separate messages for different instruments or sounds.
   myOutput = WebMidi.outputs[dropOuts.value].channels[1];
 });
 
-//Three Problems with the Code: Note On Messages are getting duplicated, The Chord Context function is getting overrided, I want to be able to change between the Progression and Chord Generator
+//This bonus section controls the status check function. Whenever the button on the webpage is clicked, a bunch of information anbout
+//the current state of the software is logged in the console. This helps me see what the code thinks things are, and if there are any discrepancies
+//between whats being shown on the webpage and what's happening under the hood.
 
 statusCheck.addEventListener("click", function () {
   console.log(`Progression Generator? ${progGenOn}`);
   console.log(`Chord Quality: ${quality}`);
   console.log(`Key Context: ${keyContextToggle}`);
   console.log(`Key Context Key: ${keyContextSelection}`);
+  console.log(`The Tempo of the Chord Progression is ${tempoInput}`);
 });
